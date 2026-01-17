be715f81689a5969b962642ba1550ec467f94ffb
theangelskies <theangelskies13@yahoo.com>
2026-01-17 10:53:00 +0100

    changed to default

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -7,7 +7,7 @@
 
 function calculateMedian(list) {
   const middleIndex = Math.floor(list.length / 2);
-  const median = list.filter((it) => !isNaN(it)).splice(middleIndex, 1)[0];
+  const median = list.splice(middleIndex, 1)[0];
   return median;
 }
 
