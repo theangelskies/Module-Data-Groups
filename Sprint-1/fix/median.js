fe1592f9c72d86f5639570e12a76759fdeae6468
theangelskies <theangelskies13@yahoo.com>
2026-01-22 20:14:16 +0100

    If the input is not an array, return null

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -5,10 +5,11 @@
 // Hint: Please consider scenarios when 'list' doesn't have numbers (the function is expected to return null)
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
-function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
-}
 
+function calculateMedian(list) {
+  if (!Array.isArray(list)) {
+    return null;
+  }}
+
+  
 module.exports = calculateMedian;
