64da8d737ea87e9aafc8a3d370f090a964b0298f
theangelskies <theangelskies13@yahoo.com>
2026-01-22 20:21:46 +0100

    even-length arrays

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -19,4 +19,8 @@ if (numbers.length === 0) {
 const sortedNumbers = [...numbers].sort((a, b) => a - b);
 const middleIndex = Math.floor(sortedNumbers.length / 2);
 
+if (sortedNumbers.length % 2 === 0) {
+  return (sortedNumbers[middleIndex - 1] + sortedNumbers[middleIndex]) / 2;
+}
+
 module.exports = calculateMedian;
