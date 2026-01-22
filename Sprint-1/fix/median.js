d99199257f02b686c104f984ccda26f1a2f3461c
theangelskies <theangelskies13@yahoo.com>
2026-01-22 20:20:37 +0100

    Find the middle index

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -17,5 +17,6 @@ if (numbers.length === 0) {
   return null;
 }
 const sortedNumbers = [...numbers].sort((a, b) => a - b);
+const middleIndex = Math.floor(sortedNumbers.length / 2);
 
 module.exports = calculateMedian;
