a4c481cd6a2e6d8f173487030a2fac01e99c338c
theangelskies <theangelskies13@yahoo.com>
2026-01-22 20:50:03 +0100

    Add logic to filter unique items from array

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -2,6 +2,13 @@ function dedupe(array) {
   const seen = new Set();
   const result = [];
 
+  for (const item of array) {
+    if (!seen.has(item)) {
+      seen.add(item);
+      result.push(item);
+    }
+  }
+
   return result;
 }
 
