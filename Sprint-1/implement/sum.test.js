eb650ff164e7a7b1bdb358444da54cfa40d98ecb
theangelskies <theangelskies13@yahoo.com>
2026-01-23 10:29:45 +0100

    test empty array

--- Sprint-1/implement/sum.test.js
+++ Sprint-1/implement/sum.test.js
@@ -13,8 +13,9 @@ const sum = require("./sum.js");
 // Given an empty array
 // When passed to the sum function
 // Then it should return 0
-test.todo("given an empty array, returns 0")
-
+test("given an empty array, returns 0", () => {
+  expect(sum([])).toBe(0);
+});
 // Given an array with just one number
 // When passed to the sum function
 // Then it should return that number
