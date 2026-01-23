401651a0982c4f3819fa38a96b6ace31f0c3d0bf
theangelskies <theangelskies13@yahoo.com>
2026-01-23 10:19:58 +0100

    refactor max.js

--- Sprint-1/implement/max.js
+++ Sprint-1/implement/max.js
@@ -1,4 +1,15 @@
 function findMax(elements) {
+  let max = -Infinity;
+
+  for (const element of elements) {
+    if (typeof element === "number") {
+      if (element > max) {
+        max = element;
+      }
+    }
+  }
+
+  return max;
 }
 
 module.exports = findMax;
