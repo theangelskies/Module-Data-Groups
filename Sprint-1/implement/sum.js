d15bfb317bcc735c1e54388c1962c3813f91287c
theangelskies <theangelskies13@yahoo.com>
2026-01-23 10:42:48 +0100

    refactor sum.js

--- Sprint-1/implement/sum.js
+++ Sprint-1/implement/sum.js
@@ -1,4 +1,13 @@
 function sum(elements) {
+  let total = 0;
+
+  for (const element of elements) {
+    if (typeof element === "number") {
+      total += element;
+    }
+  }
+
+  return total;
 }
 
 module.exports = sum;
