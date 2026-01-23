e2d77f491e32b3649b39fb4f93c5effa8262d24e
theangelskies <theangelskies13@yahoo.com>
2026-01-23 10:09:52 +0100

    Test to return -Infinity

--- Sprint-1/implement/max.test.js
+++ Sprint-1/implement/max.test.js
@@ -16,7 +16,9 @@ const findMax = require("./max.js");
 // When passed to the max function
 // Then it should return -Infinity
 // Delete this test.todo and replace it with a test.
-test.todo("given an empty array, returns -Infinity");
+test("given an empty array, returns -Infinity", () => {
+  expect(findMax([])).toBe(-Infinity);
+});
 
 // Given an array with one number
 // When passed to the max function
