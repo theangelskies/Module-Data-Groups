f3bd4c41fecf2930b8449fa7e2567ce297b683d0
theangelskies <theangelskies13@yahoo.com>
2026-01-28 00:32:57 +0100

    Add explanation for TypeError in author object iteration

--- Sprint-2/debug/author.js
+++ Sprint-2/debug/author.js
@@ -1,4 +1,5 @@
 // Predict and explain first...
+//  TypeError: author is not iterable, for...of loop only works with iterables.
 
 // This program attempts to log out all the property values in the object.
 // But it isn't working. Explain why first and then fix the problem
