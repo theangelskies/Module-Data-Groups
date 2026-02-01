d69be4493e7122f6a7fa3596b6281455a05cee01
theangelskies <theangelskies13@yahoo.com>
2026-02-01 13:50:38 +0100

    refactor lookup.js

--- Sprint-2/implement/lookup.js
+++ Sprint-2/implement/lookup.js
@@ -1,5 +1,16 @@
-function createLookup() {
-  // implementation here
+function createLookup(pairs) {
+  // Guard against invalid input
+  if (!Array.isArray(pairs)) {
+    return {};
+  }
+
+  return pairs.reduce((lookup, pair) => {
+    if (Array.isArray(pair) && pair.length === 2) {
+      const [countryCode, currencyCode] = pair;
+      lookup[countryCode] = currencyCode;
+    }
+    return lookup;
+  }, {});
 }
 
 module.exports = createLookup;
