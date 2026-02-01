8d989620a407df6c9d26471bc743187be122cbc9
theangelskies <theangelskies13@yahoo.com>
2026-02-01 23:12:28 +0100

    test  to count a single item"

--- Sprint-2/implement/tally.test.js
+++ Sprint-2/implement/tally.test.js
@@ -30,6 +30,11 @@ test("tally on an empty array returns an empty object", () => {
 // Given an array with duplicate items
 // When passed to tally
 // Then it should return counts for each unique item
+test("tally counts a single item", () => {
+  expect(tally(["a"])).toEqual({
+    a: 1,
+  });
+});
 
 // Given an invalid input like a string
 // When passed to tally
