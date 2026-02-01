aa17ca70bf754d1b753e87c5ccc9b549eb9c86a9
theangelskies <theangelskies13@yahoo.com>
2026-02-01 13:40:13 +0100

    refactor contains.js

--- Sprint-2/implement/contains.js
+++ Sprint-2/implement/contains.js
@@ -1,3 +1,11 @@
-function contains() {}
+function contains(obj, property) {
+  // Return false if obj is not a plain object or is null
+  if (obj === null || typeof obj !== "object" || Array.isArray(obj)) {
+    return false;
+  }
+
+  // Check if the property exists on the object
+  return Object.prototype.hasOwnProperty.call(obj, property);
+}
 
 module.exports = contains;
