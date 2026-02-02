bc2976e014e12ed826eff6af1980225b596852de
theangelskies <theangelskies13@yahoo.com>
2026-02-02 14:02:05 +0100

    refactor alarm clock timer functionality

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -2,26 +2,10 @@ let timeRemaining = 0;
 let timerInterval = null;
 
 function setAlarm() {
-  const hours = parseInt(document.getElementById("hours").value) || 0;
-  const minutes = parseInt(document.getElementById("minutes").value) || 0;
-  const seconds = parseInt(document.getElementById("seconds").value) || 0;
-
-  timeRemaining = hours * 3600 + minutes * 60 + seconds;
-
-  if (timeRemaining <= 0) {
-    alert("Please set a valid time.");
-    return;
+  // Clear any existing timer
+  if (timerInterval) {
+    clearInterval(timerInterval);
   }
-
-  clearInterval(timerInterval);
-  timerInterval = setInterval(() => {
-    timeRemaining--;
-    if (timeRemaining <= 0) {
-      clearInterval(timerInterval);
-      playAlarm();
-    }
-  }, 1000);
-}
 
 // DO NOT EDIT BELOW HERE
 
