9348dcd6baf2f9f8b7686ffcd228b8b192fbc404
theangelskies <theangelskies13@yahoo.com>
2026-02-02 14:02:00 +0100

    Implement alarm setting functionality with time validation and countdown

--- Sprint-3/alarmclock/alarmclock.js
+++ Sprint-3/alarmclock/alarmclock.js
@@ -1,7 +1,27 @@
 let timeRemaining = 0;
 let timerInterval = null;
 
-function setAlarm() {}
+function setAlarm() {
+  const hours = parseInt(document.getElementById("hours").value) || 0;
+  const minutes = parseInt(document.getElementById("minutes").value) || 0;
+  const seconds = parseInt(document.getElementById("seconds").value) || 0;
+
+  timeRemaining = hours * 3600 + minutes * 60 + seconds;
+
+  if (timeRemaining <= 0) {
+    alert("Please set a valid time.");
+    return;
+  }
+
+  clearInterval(timerInterval);
+  timerInterval = setInterval(() => {
+    timeRemaining--;
+    if (timeRemaining <= 0) {
+      clearInterval(timerInterval);
+      playAlarm();
+    }
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
