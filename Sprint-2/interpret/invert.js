4f9a36ab584e95dbaa42f9b8574d2a90867e6b72
theangelskies <theangelskies13@yahoo.com>
2026-02-02 11:45:11 +0100

    add console logs for testing invert function with multiple key-value pairs

--- Sprint-2/interpret/invert.js
+++ Sprint-2/interpret/invert.js
@@ -21,6 +21,13 @@ console.log(invert({ a: 1 }));
 // { key: 1 }
 
 // b) What is the current return value when invert is called with { a: 1, b: 2 }
+console.log(invert({ a: 1, b: 2 }));
+// {
+//   key: 1;
+// }
+// {
+//   key: 2;
+// }
 
 // c) What is the target return value when invert is called with {a : 1, b: 2}
 
