e4b33ab9f81d14a5917143f58a3ee8e68962d7e9
theangelskies <theangelskies13@yahoo.com>
2026-02-03 19:41:11 +0100

    style: format image array and reorder comments in slideshow.js

--- Sprint-3/slideshow/slideshow.js
+++ Sprint-3/slideshow/slideshow.js
@@ -1,8 +1,14 @@
 const images = [
-    "./assets/cute-cat-a.png",
-    "./assets/cute-cat-b.jpg",
-    "./assets/cute-cat-c.jpg",
+  "./assets/cute-cat-a.png",
+  "./assets/cute-cat-b.jpg",
+  "./assets/cute-cat-c.jpg",
 ];
 
-
 // Write your code here
+
+let currentIndex = 0;
+
+// Grab elements
+const img = document.getElementById("carousel-img");
+const forwardBtn = document.getElementById("forward-btn");
+const backwardBtn = document.getElementById("backward-btn");
