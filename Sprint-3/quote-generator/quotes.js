b881df4a0b84256611fc1b19eda51c74da66c3f2
theangelskies <theangelskies13@yahoo.com>
2026-02-03 12:41:11 +0100

    Remove unused pickFromArray function from quotes.js

--- Sprint-3/quote-generator/quotes.js
+++ Sprint-3/quote-generator/quotes.js
@@ -55,9 +55,6 @@ window.addEventListener("load", showRandomQuote);
 // pickFromArray(['a','b','c','d'])     // maybe returns 'c'
 
 // You don't need to change this function
-function pickFromArray(choices) {
-  return choices[Math.floor(Math.random() * choices.length)];
-}
 
 // A list of quotes you can use in your app.
 // DO NOT modify this array, otherwise the tests may break!
