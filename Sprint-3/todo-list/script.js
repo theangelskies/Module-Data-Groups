3f9b91365aa6f2d3c745c7461acbdf96e3d24cab
theangelskies <theangelskies13@yahoo.com>
2026-02-07 14:16:44 +0100

    add "remove  completed Btn"

--- Sprint-3/todo-list/script.js
+++ Sprint-3/todo-list/script.js
@@ -57,13 +57,17 @@ addBtn.addEventListener("click", () => {
 });
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
-function addNewTodo(event) {
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
-  event.preventDefault();
-  // Write your code here... and remember to reset the input field to be blank after creating a todo!
-}
-
+// The code below prevents the page from refreshing when we click the 'Add Todo' button.
+// Write your code here... and remember to reset the input field to be blank after creating a todo
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
-function deleteAllCompletedTodos() {
-  // Write your code here...
-}
+
+// ---------- Remove all completed ----------
+removeCompletedBtn.addEventListener("click", () => {
+  const items = [...todoList.querySelectorAll("li")];
+
+  items.forEach((li) => {
+    if (li.style.textDecoration === "line-through") {
+      li.remove();
+    }
+  });
+});
