a08e3a7f08cd9a8889f5147f83434f97d995aedf
theangelskies <theangelskies13@yahoo.com>
2026-02-07 14:11:35 +0100

    created element

--- Sprint-3/todo-list/script.js
+++ Sprint-3/todo-list/script.js
@@ -1,16 +1,44 @@
-function populateTodoList(todos) {
-  let list = document.getElementById("todo-list");
-  // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
+// Get elements
+const todoList = document.querySelector("#todo-list");
+const addBtn = document.querySelector(".btn");
+const input = document.querySelector("#todoInput");
+const removeCompletedBtn = document.querySelector("#remove-all-completed");
+
+// ---------- Helper to create a todo ----------
+function createTodo(text) {
+  const li = document.createElement("li");
+  li.innerText = text;
+
+  // Tick icon
+  const tick = document.createElement("i");
+  tick.classList.add("fa", "fa-check");
+
+  tick.addEventListener("click", () => {
+    if (li.style.textDecoration === "line-through") {
+      li.style.textDecoration = "";
+    } else {
+      li.style.textDecoration = "line-through";
+    }
+  });
+
+  // Trash icon
+  const trash = document.createElement("i");
+  trash.classList.add("fa", "fa-trash");
+
+  trash.addEventListener("click", () => {
+    li.remove();
+  });
+
+  li.appendChild(tick);
+  li.appendChild(trash);
+
+  return li;
 }
 
+// Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
 // These are the same todos that currently display in the HTML
 // You will want to remove the ones in the current HTML after you have created them using JavaScript
-let todos = [
-  { task: "Wash the dishes", completed: false },
-  { task: "Do the shopping", completed: false },
-];
 
-populateTodoList(todos);
 
 // This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
 function addNewTodo(event) {
