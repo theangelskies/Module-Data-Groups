1ce2b7f2f813d0edd6fe4bb0159542b323731e59
theangelskies <theangelskies13@yahoo.com>
2026-02-24 14:39:24 +0100

    refactor dedupe function to use Set for cleaner and more efficient deduplication

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,15 +1,5 @@
 function dedupe(array) {
-  const seen = new Set();
-  const result = [];
-
-  for (const item of array) {
-    if (!seen.has(item)) {
-      seen.add(item);
-      result.push(item);
-    }
-  }
-
-  return result;
+  return [...new Set(array)];
 }
 
 module.exports = dedupe;
